62ea2fb62712e2b21466e3c1b1e870750db3808c
Lampros Papadimitriou <papadimitriou@tyntec.com>
2018-09-09 00:36:24 +0200

    adding animation, actions slot and modifying demo

--- DemoPage.vue
+++ DemoPage.vue
@@ -11,12 +11,21 @@
 <template>
 <div class="example">
   <h1>Collapsible panel demo</h1>
-  <CollapsiblePanel>
-    This is not visible
-  </CollapsiblePanel>
-  <CollapsiblePanel :expanded="true">
+  <CollapsiblePanel  class="example-2" :expanded="true">
     <div slot="header">Hello there</div>
-    This is visible
+    <div slot="actions"><button>Hello</button></div>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+     mollit anim id est laborum.
   </CollapsiblePanel>
 </div>
 </template>
+
+<style>
+  .example-2 {
+    max-width: 40rem;
+  }
+</style>
--- src/CollapsiblePanel.vue
+++ src/CollapsiblePanel.vue
@@ -5,8 +5,26 @@ export default {
     expanded: {
       type: Boolean,
       default: false,
+    },
+    transitionBody: {
+      type: String,
+      default: 'fade',
+    },
+    transitionHeader: {
+      type: String,
+      default: 'fade-bottom',
     }
   },
+  data() {
+    return {
+      isExpanded: false,
+    };
+  },
+  methods: {
+    toggle() {
+      this.isExpanded = !this.isExpanded;
+    }
+  }
 };
 </script>
 <template>
@@ -14,19 +32,81 @@ export default {
     <div class="collapsible-panel__header">
       <button
         class="collapsible-panel__headline"
-        :aria-expanded="expanded"
+        :aria-expanded="isExpanded"
         aria-controls="collapsible-0"
+        v-on:click="toggle"
       >
         <slot name="header"></slot>
       </button>
+      <transition :name="transitionHeader">
+        <div
+          class="collapsible-panel__actions"
+          v-if="isExpanded"
+        >
+          <slot name="actions"></slot>
+        </div>
+      </transition>
     </div>
-    <div v-if="expanded">
-      <slot/>
-    </div>
+    <transition :name="transitionBody">
+      <div
+        id="collapsible-0"
+        :aria-hidden="!isExpanded"
+        class="collapsible-panel__container"
+        v-if="isExpanded"
+      >
+        <slot/>
+      </div>
+    </transition>
   </div>
 </template>
 
 <style>
   .collapsible-panel {
+  }
+
+  .collapsible-panel__actions {
+    flex-grow: 1;
+    text-align: right;
+  }
+
+  .collapsible-panel__container {
+    margin: 0;
+    padding: 0.3rem;
+  }
+
+  .collapsible-panel__header {
+    border-bottom: 1px solid #666;
+    display: flex;
+    padding: 0.3rem;
+  }
+
+  .collapsible-panel__headline {
+    background-color: transparent;
+    border: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  .fade-bottom-enter-active, .fade-bottom-leave-active {
+    transition: transform .33s  ease-out;
+    height: auto;
+    transform: scaleY(1);
+    transform-origin: bottom;
+  }
+
+  .fade-bottom-enter, .fade-bottom-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    transform: scaleY(0);
+  }
+
+
+  .fade-enter-active, .fade-leave-active {
+    transition: transform .33s  ease-out;
+    height: auto;
+    transform: scaleY(1);
+    transform-origin: top;
+  }
+
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    transform: scaleY(0);
   }
 </style>
